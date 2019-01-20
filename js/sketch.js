a80a40e434aabec906411a5527ff3534f0eb8c54
Josie11486 <31413862+Josie11486@users.noreply.github.com>
2019-01-20 16:10:49 -0500

    Total Update

---
+++ js/sketch.js
@@ -0,0 +1,19 @@
+// var y = 100;
+//
+// function setup() {
+//   var canvas = createCanvas(800, 200);
+//   canvas.parent('sketch-holder');
+//   background(255, 0, 200);
+// }
+//
+// function draw() {
+//   fill(255);
+//   textSize(50);
+//   text('W', 100, y);
+//   text('E', 155, y);
+//   text('L', 205, y);
+//   text('C', 250, y);
+//   text('O', 300, y);
+//   text('M', 350, y);
+//   text('E', 400, y);
+// }
